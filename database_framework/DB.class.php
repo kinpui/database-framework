92e4b6d373a632ea6be0f12f0726d71bbff55ab9
kin <cheongkinpui@163.com>
2015-12-30 20:28:52 +0800

    add where_clause , data_implode

--- database_framework/DB.class.php
+++ database_framework/DB.class.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *
  * ass database framework
@@ -234,11 +235,7 @@ class DB{
   
   }
 
-  /**
-   *
-   * select_context
-   *
-   **/
+   //**** select_context
 
   /**
    *
@@ -248,31 +245,106 @@ class DB{
    * @param $table  string  表名
    * @param $data   array   插入到表里的信息数组
    *
-   * return 插入的最后一条信息记录
+   * return 插入的数据条数
    *
    **/
   public function insert( $table,$datas ){
 
-    //插入的数据返回结果
-    $lastid = array();
+    //插入的数据返回影响条数
+    $insert_result = 0;
 
     //确认信息数组是array
     if( is_array( $datas ) ){
-      
-      foreach( $datas as $key => $value ){
-      
-        echo $key,'===',$value,'<hr>';
-          
-        **********************************
+
+      //检测关联或索引数组
+      if( !isset( $datas[ 0 ] ) ){
+
+        //关联数组
+        //转换为索引数组
+        $datas = array( $datas );
       
       }
+      foreach( $datas as $data ){
 
+        /**
+         *
+         * @param   array   $values  插入数据库的值 
+         * @param   array   $columns  插入数据库的字段
+         *
+         **/
+
+        $values  = array();
+        $columns = array();
+
+        if( is_array( $data ) ){
+
+          //遍历三维数组
+          foreach( $data as $key => $value ){
+
+            //将key 设置为插入的数据库字段
+            //给每个key添加双引号
+            array_push( $columns , $key );
+
+            //判断插入数据库值的类型
+            switch( gettype( $value ) ){
+              
+              case  'NULL':
+              case  'null':
+                $values[] = 'NULL';
+                break;
+              
+              case  'array':
+                preg_match("/\(JSON\)\s*([\w]+)/i", $key, $column_match);
+
+                $values[] = isset( $column_match[ 0 ] ) ?
+                            $this->quote( json_encode( $value ) ):
+                            $this->quote( serialize( $value ) );
+                break;
+
+              case  'boolean':
+                $values[] = $value ? '1' : '0';
+                break;
+
+              case  'integer':
+              case  'double' :
+              case  'string' :
+                $values[] = $this->fn_quote( $key,$value );
+                break;
+            }
+
+          }
+          
+          $sql = 'INSERT INTO `'.$this->prefix .$table .'` ('. implode( ',' , $columns ).') VALUES('. implode( ',' , $values ) .');';
+          if( $this->exec( $sql ) ){
+            $insert_result += 1;
+          }
+        }
+      }
+
+      return $insert_result;
     }
-
   }
 
 
+  /**
+   *
+   * delete
+   * 删除记录
+   *
+   * @param $table  string  表名
+   * @param $where  array   删除的条件
+   *
+   * return 成功与否
+   *
+   *用法: 
+   *
+   **/
 
+   public function delete( $table,$where ){
+
+     $sql = 'DELETE FROM `'.$this->prefix .$table . '` '.$this->where_clause( $where );
+     echo $sql;
+   }
 
   //end ( select 查询方法 )
 
@@ -294,6 +366,26 @@ class DB{
   
   }
 
+  /**
+   *
+   * 为keyt 添加双引号
+   * &
+   * 过滤特殊字符串
+   *
+   *  ( JSON or #  = '')
+   *  ( . = "." )
+   *
+   **/
+	public function column_quote($string)
+  {
+    /**
+     *
+     * 过滤 @param $string 中的所有# 或 (JSON) 字符
+     * 将   @param $strubg 中的. 替换成 "."
+     *
+     **/
+		return '"' . str_replace('.', '"."', preg_replace('/(^#|\(JSON\)\s*)/', '', $string)) . '"';
+	}
 
   /**
    *
@@ -330,36 +422,365 @@ class DB{
    *
    *? 241
    **/
-  protected function inner_conjunct( $data,$conjunctor,$outer_conjunctor ){
+  protected function inner_connector( $data,$connector,$outer_connector ){
 
     $haystack = array();
     
-    if( empty( $conjunctor ) ){
-      $conjunctor = '';
+    if( empty( $connector ) ){
+      $connector = '';
     }
 
     if( is_array( $data )){
       foreach( $data as $value ){
-        $haystack[] = '('. $this->data_implode( $value , $conjunctor ).')';
+        $haystack[] = '('. $this->data_implode( $value , $connector ).')';
       }
 
-      return implode( $outer_conjunctor . ' ' , $haystack );
+      return implode( $outer_connector . ' ' , $haystack );
     }
   
   }
 
 
   /**
-   *? end
+   * 
+   * 识别使用SQL函数来处理数据。
+   *  （在字段前加入#号）
+   *
    **/
+
   protected function fn_quote( $column,$string ){
 
-    return ( strpos( $column , '#' ) == 0 && preg_match( '/^[A-Z0-9\_]*\([^)]*\)$/',$string ) )?
-      $string:
-      $this->quote( $string );
+    /**
+     *
+     * 匹配 column 的第一个字符串是否为 '#'
+     * 匹配 函数
+     *
+     * @param $column string  插入数据库的字符名
+     * @param $string string  插入数据库的数据
+     *
+     * 如果$column 首字母是# 而且 插入的$string 中格式是大写A-Z0-9_和一对小括号
+     * 组成。那么就认为他是需要处理的函数, 返回原函数名称，
+     * 否则将$string当字符串处理
+     *
+     **/
 
+    return  ( strpos( $column , '#' ) === 0 && preg_match( '/^[A-Z0-9\_]*\([^)]*\)$/',$string ) )?
+            $string:
+            $this->quote( $string );
   }
 
+  /**
+   *
+   * where_clause
+   * 拼接条件
+   *
+   * @param $where array 传入的条件
+   *
+   * return 转义的字符串输出
+   *
+   **/
+
+   public function where_clause( $where ){
+
+     //定义where语句
+     $where_clause = '';
+     if( is_array( $where )){
+
+       //获取array的key
+       $where_key = array_keys( $where );
+
+       //匹配AND 开始的字符串
+       $where_AND = preg_grep( "/^AND\s*#?$/i" , $where_key );
+       //匹配OR 开始的字符串
+       $where_OR  = preg_grep( "/^OR\s*#?$/i"  , $where_key );
+
+       $single_condition = array_diff_key( $where, array_flip(
+          explode( ' ', 'AND OR GROUP HAVING LIMIT LIKE MATCH')
+       ));
+
+       if( $single_condition != array() ){
+         $where_clause = ' WHERE '.$this->data_implode( $single_condition , '' );
+       }
+
+       //如果是 OR 语句
+       if( !empty( $where_OR )){
+       
+         $value = array_values( $where_OR );       
+         $where_clause = ' WHERE '.$this->data_implode( $where[ $value[ 0 ] ],' OR ' );
+       }
+
+       //如果是 AND 语句
+       if( !empty( $where_AND )){
+    
+         $value = array_values( $where_AND );       
+         $where_clause = ' WHERE '.$this->data_implode( $where[ $value[ 0 ] ],' AND ' );
+       }
+
+       //使用match 全文检索
+       if( isset( $where[ 'MATCH' ] ) ){
+
+         $MATCH = $where[ 'MATCH' ];
+
+         //如果这个 $MATCH 是个数组，并且包含有columns 和 keyword 两个值
+         if( (isset( $MATCH )) && isset( $MATCH[ 'columns' ] , $MATCH[ 'keyword' ]) ){
+
+           //拼接match
+           $where_clause .= ( $where_clause != '' ? 'AND' : 'WHERE' ).
+                            ' MATCH("' . str_replace( '.', '"."', implode( $MATCH[ 'columns' ], '","')).
+                            '") AGAINST ('.$this->quote( $MATCH[ 'keyword' ] ). ')';
+         }
+       
+       }
+
+       /**
+        *
+        * 使group by 排序
+        * 
+        **/
+       if( isset( $where[ 'GROUP' ] ) ){
+
+         //如果GROUP 
+         $where_clause .= ' GROUP BY '.$this->column_quote( $where[ 'GROUP' ] );
+
+         //如果是HAVING
+         if( (isset( $where[ 'HAVING' ] )) ){
+
+           $where_clause .= ' HAVING '.$this->data_implode( $where['HAVING'] , ' AND'); 
+         }
+       
+       }
+
+       if( isset( $where[ 'ORDER' ] ) ){
+         //匹配字符desc asc
+         $rsort = '/(^[A-Za-z0-9_\-\.]*)(\s(DESC|ASC))?/';
+         //排序方式
+         $ORDER = $where[ 'ORDER' ];
+       
+         //如果ORDER是数组
+         if( is_array( $ORDER ) ){
+           if(
+
+             isset( $ORDER[ '1' ]) &&
+             is_array( $ORDER[ 1 ])
+           ){
+
+             //FIELD
+             $where_clause .= ' ORDER BY FIELD('.$this->column_quote( $ORDER[ 0 ]).' , '.$this->array_quote( $ORDER[ 1 ]).')';
+           }else{
+
+             $stack = array();
+             foreach( $ORDER as $column ){
+
+               //match 
+               preg_match( $rsort , $column , $order_match );
+               array_push( $stack , '"' . str_ireplace( '.' , '"."', $order_match[ 1 ]).'"'.(isset( $order_match[ 3 ] ))?' '.$order_match[ 3 ]:'' );
+             }
+           }
+         }
+       }
+     
+       if( isset( $where[ 'LIMIT' ] ) ){
+
+         $LIMIT   = $where['LIMIT'];
+
+         //如果是数字
+         if( is_numeric( $LIMIT )){
+
+           //拼接单个数字
+           $where_clause .= ' LIMIT '.$LIMIT;
+         }    
+  
+         if( is_array( $LIMIT ) && is_numeric( $LIMIT[ 0 ]) && is_numeric( $LIMIT[1] ) ){
+
+           //拼接多个数字
+           $where_clause .= ' LIMIT ' . $LIMIT[0] . ',' . $LIMIT[1];
+         }
+       
+       }
+     
+     }else{
+
+      //不是数组且不为空直接拼接
+       if( $where != null ){
+         $where_clause .= ' '.$where;
+       }
+     }
+
+     //返回组成的sql where 字符串
+     return $where_clause;
+   }
+
+  /**
+   *
+   * data_implode
+   * 维数组的值转化为字符串
+   *
+   * where 安全过滤来保证
+   *
+   * @param array   $data             需要转化为字符串的数组
+   * @param string  $connector        连接字符
+   * @param string  $outer_connector  外部连接字符
+   *
+   **/
+
+   protected function data_implode( $data, $connector, $outer_connector = null ){
+
+     $wheres = array();
+
+     if( is_array( $data ))
+     {
+
+       foreach( $data as $key => $value )
+       {
+
+         $type = gettype( $value );
+
+         //匹配 AND OR 
+         //并且是数组
+         if(
+           preg_match( '/^(AND|OR)(\s+#.*)$/i' , $key , $relation_match ) && 
+           $type == 'array'
+         )
+         {
+
+           $wheres[] = 0 !== count( array_diff_key( $value, array_keys( $value ) )) ?
+             '('.$this->data_implode( $value, ' ', $relation_match[ 1 ]) . ')':
+             '('.$this->inner_connector( $value, ' ', $relation_match[ 1 ], $connector ) . ')';
+         }
+         else
+         {
+
+           //匹配以#开头
+           //匹配包括 > , < , <= , => , ! , <> 等操作符号
+           preg_match( '/(#?)([\w\.\-]+)(\[(\>|\>\=|\<|\<\=|\!|\<\>|\!?~)\])?/i' , $key , $macth );
+
+           //过滤字符
+           $column = $this->column_quote( $macth[ 2 ] );
+
+           if( isset( $macth[ 4 ] ) )
+           {
+             //取得运算符
+             $operator = $macth[ 4 ];
+
+             //判断预算法是否等于 !
+             if( $operator == '!' ){
+
+               switch( $type ){
+
+                 case 'NULL':
+                   $wheres[] = $column.' IS NOT NULL';
+                   break;
+
+                 case 'array':
+                   $wheres[] = $column . 'NOT IN (' . $this->array_quote( $value ) . ')';
+                   break;
+
+                 case 'integer':
+                 case 'double' :
+                   $wheres[] = $column . ' != '.$value;
+                   break;
+                 case 'boolean':
+                   $wheres[] = $column . ' != '.( $value ? '1' : '0' );
+                   break;
+                 case 'string' :
+                   $wheres[] = $column . ' != '.$this->fn_quote( $key, $value );
+                   break;
+               }
+             }
+
+             //判断预算法是否等于 <> 或者 ><
+             if( $operator == '<>' || $operator == '><' )
+             {
+
+               if( $type == 'array' ){
+
+                 if( $operator == '><' ){
+                   $column .= ' NOT';
+                 }
+
+                 if( is_numeric( $value[ 0 ] ) && is_numeric( $value[ 1 ] )  ){
+
+                   $wheres[] = '('.$column.' BETWEEN '.$value[ 0 ].' AND '.$value[ 1 ].')';
+                 }else{
+
+                   $wheres[] = '('.$column.' BETWEEN '.$this->quote( $value[ 0 ] ).' AND '.$this->quote( $value[ 1 ] ).')';
+                 }
+               }
+             }
+
+
+             //判断预算法是否等于 ~ 或者 !~
+             if( $operator == '~' || $operator == '!~' )
+             {
+               if( $type == 'string' )
+               {
+                 $value = array( $value );
+               }
+
+               if( !empty( $value ))
+               {
+
+                 $like_clauses = array();
+
+                 foreach( $value as $item )
+                 {
+
+                   if( preg_match( '/^(?!%).+(?<!%)$/' , $item ) )
+                   {
+                      // %vale% 
+                     $item = '%'.$item.'%';
+                   }
+
+                   $like_clauses[] = $column . ( $operator === '!~' ? ' NOT':'' ) . ' LIKE '.$this->fn_quote( $key , $item );
+                 }
+
+                 $wheres[] = implode( ' OR',$like_clauses );
+               }
+             }
+
+             //< > <= >= 存在数组之中
+             if( in_array( $operator , array( '>', '<', '>=', '<=' ) ) )
+             {
+
+               if( is_numeric( $value )){//$value 是数值
+
+                 $wheres[] = $column . ' ' .$operator . ' ' .$value;
+               }elseif( strpos( $key , '#' ) ){//$key 中 包含有 #
+
+                 $wheres[] = $column . ' ' .$operator . ' '.$this->fn_quote( $key ,$value );
+               }else{
+
+                 $wheres[] = $column . ' ' .$operator . ' '.$this->quote( $value );
+               }
+             }
+           //end if match[ 4 ]
+           //如果没有匹配到有操作符
+           }else{
+               switch( $type ){
+                 case 'NULL'   :     //如果type 为 NULL
+                   $wheres[] = $column. ' IS NULL '; 
+                   break;
+                 case 'array'  :    //如果type 为 array
+                   $wheres[] = $column. ' IN ('.$this->array_quote( $value ). ')';
+                   break;
+                 case 'integer':
+                 case 'double':     //如果为数值
+                   $wheres[] = $column. ' = '.$value;
+                   break;
+                 case 'boolean':
+                   $wheres[] = $column. ' = '.( $value ? '1' : '0' ); 
+                   break;
+                 case 'string' :    //字符串
+                   $wheres[] = $column. ' = '.$this->fn_quote( $key, $value );
+                   break;
+               }
+             }
+
+         }//end else
+       }//end foreach
+     }//end if array
+     return implode( $connector.' ', $wheres );
+   }//end data_implode
   /**
    *
    * select 
@@ -487,7 +908,7 @@ class DB{
 
     //输出错误的代码
     echo '出错的sql语句:',$this->last_query();
-  
+
   }
 
 
